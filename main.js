61355e6342db34b3c94cbdc103edbee0dcd55b5f
Penna92 <giacomopenna92@gmail.com>
2022-03-25 14:48:00 +0100

    main.js: commented all console.log

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let fa_bars = document.querySelector('.fa-bars');
 fa_bars.setAttribute('id', 'hamburger');
 let hamburger = document.getElementById("hamburger");
-console.log(hamburger);
+// console.log(hamburger);
 
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 hamburgerMenu.setAttribute('id', 'hamburger-menu');
 let hamburgerMenuShow = document.getElementById('hamburger-menu');
-console.log(hamburgerMenuShow);
+// console.log(hamburgerMenuShow);
 
 hamburger.addEventListener("click", function() {
     hamburgerMenuShow.style["display"] = "block";
@@ -14,7 +14,7 @@ hamburger.addEventListener("click", function() {
 
 let close = document.querySelector('.close');
 close.setAttribute('id', 'close');
-console.log(close);
+// console.log(close);
 
 close.addEventListener("click", function() {
     hamburgerMenuShow.style["display"] = "none";
