d9216a5c17b2e55f6f58130b24295a1ac6e0e6e1
Penna92 <giacomopenna92@gmail.com>
2022-03-25 14:45:51 +0100

    Imported files and added content to main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+let fa_bars = document.querySelector('.fa-bars');
+fa_bars.setAttribute('id', 'hamburger');
+let hamburger = document.getElementById("hamburger");
+console.log(hamburger);
+
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.setAttribute('id', 'hamburger-menu');
+let hamburgerMenuShow = document.getElementById('hamburger-menu');
+console.log(hamburgerMenuShow);
+
+hamburger.addEventListener("click", function() {
+    hamburgerMenuShow.style["display"] = "block";
+});
+
+let close = document.querySelector('.close');
+close.setAttribute('id', 'close');
+console.log(close);
+
+close.addEventListener("click", function() {
+    hamburgerMenuShow.style["display"] = "none";
+});
+
